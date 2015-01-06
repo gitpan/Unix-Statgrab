026e093f0070a3051f13f96d7990d5a2de51be60
Jens Rehsack <rehsack@cpan.org>
2015-01-06 15:30:43 +0000

    Import of REHSACK/Unix-Statgrab-0.106.tar.gz from CPAN.
    gitpan-cpan-distribution: Unix-Statgrab
    gitpan-cpan-version:      0.106
    gitpan-cpan-path:         REHSACK/Unix-Statgrab-0.106.tar.gz
    gitpan-cpan-author:       REHSACK
    gitpan-cpan-maturity:     released

--- lib/Unix/Statgrab.pm
+++ lib/Unix/Statgrab.pm
@@ -85,7 +85,7 @@ my @constants_names = (
 	@constants_names
     );
 
-$VERSION = '0.105_001';
+$VERSION = '0.106';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
@@ -619,7 +619,7 @@ Jens Rehsack, E<lt>rehsack AT cpan.orgE<gt>
 
 Copyright (C) 2004-2005 by Tassilo von Parseval
 
-Copyright (C) 2012-2013 by Jens Rehsack
+Copyright (C) 2012-2015 by Jens Rehsack
 
 This library is free software; you can redistribute it and/or modify it under
 the terms of the GNU Lesser General Public License as published by the Free
